7dec135a6803270c91bd7a02de02b2fab7acd8df
Adam Ploof <adamploof@hotmail.com>
2025-10-13 23:18:44 -0400

    add basic navigation and views for listing goals and progress entries

--- ProgressCapture.Web/Controllers/Web/GoalController.cs
+++ ProgressCapture.Web/Controllers/Web/GoalController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 using ProgressCapture.Web.Data;
 using ProgressCapture.Web.Models;
 using ProgressCapture.Web.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProgressCapture.Web.Controllers;
 
@@ -12,6 +14,32 @@ public class GoalController : Controller {
 
     public GoalController(ProgressCaptureDbContext context) {
         _context = context;
+    }
+
+    [HttpGet("{goalId}", Name = "ViewGoal")]
+    public async Task<IActionResult> View(int goalId) {
+        Goal? goal = await _context.Goals.FindAsync(goalId);
+        if (goal == null) {
+            return NotFound();
+        }
+
+        return View(new GoalViewModel() {
+            Name = goal.Name,
+            Description = goal.Description
+        });
+    }
+
+    [HttpGet("list", Name = "ListGoals")]
+    public async Task<IActionResult> List() {
+        List<GoalViewModel> goals = await _context.Goals.Select(g =>
+            new GoalViewModel() {
+                Id = g.Id,
+                Name = g.Name,
+                Description = g.Description,
+            }
+        ).ToListAsync();
+
+        return View(new GoalListViewModel() {Goals = goals});
     }
 
     [HttpGet("add", Name = "AddGoal")]
---
+++ ProgressCapture.Web/ViewModels/GoalListViewModel.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+namespace ProgressCapture.Web.ViewModels;
+
+public class GoalListViewModel {
+    public List<GoalViewModel> Goals { get; set; } = [];
+}
